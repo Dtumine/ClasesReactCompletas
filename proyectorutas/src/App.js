b9aa6941bfe566964471312357837844c0684bc3
Damian <damiantumine@hotmail.com>
2024-11-14 21:50:37 -0300

    backup1411

--- proyectorutas/src/App.js
+++ proyectorutas/src/App.js
@@ -1,19 +1,20 @@
 import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './Componentes/Login';
-import Registro from './Componentes/Registro';
 import Home from './Componentes/Home'; // Esta puede ser la tercera ruta o la página principal 
-import NavBar from './Componentes/NavBar'
+import NavBar from './Componentes/NavBar';
+import LoginForm from './Componentes/LoginForm';
+import RegisterForm from './Componentes/RegisterForm';
 
 function App() {
+
   return ( 
      
     <Router> 
       <NavBar/>
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Registro />} />
+        <Route path="/loginForm" element={<LoginForm />} />
+        <Route path="/register" element={<RegisterForm />} />
         <Route path="/" element={<Home />} />
       </Routes>
     </Router> 
--- proyectorutas/src/Componentes/NavBar.js
+++ proyectorutas/src/Componentes/NavBar.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 function Navbar() {
   return (
     <nav>
-      <Link to="/login">Login</Link>
+      <Link to="/loginForm">Login</Link>
       <Link to="/register">Register</Link>
       <Link to="/">Home</Link>
     </nav>
